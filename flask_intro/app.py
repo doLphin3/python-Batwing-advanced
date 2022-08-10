6a54afde466361310955fb9125bd771e0410f043
Andrii Basov <an23basov@gmail.com>
2022-08-10 22:22:35 +0300

    deleting 'flask-intro' directory after false commit

--- flask_intro/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-with app.app_context():
-    from routes.main import *
-
-if __name__ == "__main__":
-    app.run(port=5000,  host='0.0.0.0', debug=True)
